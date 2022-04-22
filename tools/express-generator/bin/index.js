5efa8de6c930067461dbe36851037b86681396a0
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2022-04-22 15:39:48 -0300

    add cli

--- tools/express-generator/bin/index.js
+++ tools/express-generator/bin/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+#!/usr/bin/env node
+
+const lib = require("../dist/express-generator.js");
+
+// const yargs = require("yargs");
+// const usage = "\nUsage: tran <lang_name> sentence to be translated";
+// const options = yargs
+//   .usage(usage)
+//   .option("build", {
+//     describe: "Build your express app.",
+//     type: "boolean",
+//     demandOption: false,
+//   })
+//   .option("watch", {
+//     describe: "Develop your express app.",
+//     type: "boolean",
+//     demandOption: false
+//   })
+//   .help(true)
+//   .argv;
+
+lib.compile();
