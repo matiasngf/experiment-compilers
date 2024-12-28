6199ed47828d998f1ad23e96b5248616a3099df0
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2024-12-28 12:44:09 -0300

    add example with player

--- fiber/react-midi-fiber/example/src/App.tsx
+++ fiber/react-midi-fiber/example/src/App.tsx
@@ -1,41 +1,83 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
+import * as Tone from 'tone'
+import { Midi } from '@tonejs/midi'
 
 import { render } from 'react-midi-fiber'
 
 import { SpaceAdventure } from './music'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Player() {
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  const [parsedMidi, setParsedMidi] = useState<Midi | null>(null)
+
+  const isPlayingRef = useRef(isPlaying)
+  isPlayingRef.current = isPlaying
+
+  // load midi
 
   useEffect(() => {
     render(<SpaceAdventure />, {
       onComplete: writer => {
         console.log(writer)
+
+        // parse midi
+        const midiData = writer.buildFile()
+        const parsedMidi = new Midi(midiData)
+        setParsedMidi(parsedMidi)
+        setLoading(false)
       }
     })
   }, [])
 
+  // play midi
+
+  const synthsRef = useRef<Tone.PolySynth[]>([])
+
+  useEffect(() => {
+    if (!parsedMidi) return
+    if (isPlaying) {
+      const now = Tone.now() + 0.01
+
+      parsedMidi.tracks.forEach(track => {
+        const synth = new Tone.PolySynth(Tone.Synth, {
+          envelope: {
+            attack: 0.02,
+            decay: 0.1,
+            sustain: 0.3,
+            release: 1
+          }
+        }).toDestination()
+        synthsRef.current.push(synth)
+        // schedule all notes
+        track.notes.forEach(note => {
+          synth.triggerAttackRelease(note.name, note.duration, note.time + now, note.velocity)
+        })
+      })
+    } else {
+      while (synthsRef.current.length) {
+        const synth = synthsRef.current.shift()
+        synth?.disconnect()
+      }
+    }
+  }, [parsedMidi, isPlaying])
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>i
+      {loading && <div>Loading...</div>}
+      <button id="play-button" onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? 'Stop' : 'Play'} Music
+      </button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Player />
     </>
   )
 }
