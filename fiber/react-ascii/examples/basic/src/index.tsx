156a9a35eb1ff6ca814d0e57c21cdf06f9cab75b
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-03-20 11:38:20 +0100

    update example

--- fiber/react-ascii/examples/basic/src/index.tsx
+++ fiber/react-ascii/examples/basic/src/index.tsx
@@ -1,5 +1,32 @@
+import fs from 'fs'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import { render } from 'react-ascii'
 
-import { App } from './app'
+const carImage = fs.readFileSync('./src/car.png')
 
-render((<App />) as any)
+const App = () => {
+  const limitMax = 70
+  const limitMin = 18
+
+  const [x, setX] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setX(x => x + 2)
+    }, 100)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  const realX = (x % (limitMax + limitMin)) - limitMin
+
+  return <asciiImage src={carImage} position={{ x: realX, y: 0 }} />
+}
+
+render(<App />, {
+  height: 30,
+  width: 70
+})
