6e3883b2c0b153de84b44304f144a371a78b414a
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-03-20 11:43:17 +0100

    update example

--- fiber/react-ascii/examples/basic/src/index.tsx
+++ fiber/react-ascii/examples/basic/src/index.tsx
@@ -1,32 +1,30 @@
 import fs from 'fs'
-import { useEffect } from 'react'
-import { useState } from 'react'
 import { render } from 'react-ascii'
 
 const carImage = fs.readFileSync('./src/car.png')
 
 const App = () => {
-  const limitMax = 70
-  const limitMin = 18
-
-  const [x, setX] = useState(0)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setX(x => x + 2)
-    }, 100)
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
-
-  const realX = (x % (limitMax + limitMin)) - limitMin
-
-  return <asciiImage src={carImage} position={{ x: realX, y: 0 }} />
+  return <asciiImage src={carImage} position={{ x: 0, y: 0 }} />
 }
 
 render(<App />, {
   height: 30,
   width: 70
 })
+
+// import { useEffect, useState } from 'react'
+// const limitMax = 70
+// const limitMin = 18
+
+// const [x, setX] = useState(0)
+
+// useEffect(() => {
+//   const interval = setInterval(() => {
+//     setX(x => x + 2)
+//   }, 100)
+
+//   return () => {
+//     clearInterval(interval)
+//   }
+// }, [])
+// const realX = (x % (limitMax + limitMin)) - limitMin
